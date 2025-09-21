7aed9a1d6af2b236d37848eb3117ed55ac345762
mohsensami <mohsensami18727@gmail.com>
2025-09-21 19:43:08 +0330

    layout and home page

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import "@/assets/styles/globals.css";
+const MAinLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main className="">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MAinLayout;
