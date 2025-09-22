098f44515707516bb2aac5fd333a107152b0b186
mohsensami <mohsensami18727@gmail.com>
2025-09-22 20:07:38 +0330

    Navbar added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse",
@@ -9,6 +10,7 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main className="">{children}</main>
       </body>
     </html>
