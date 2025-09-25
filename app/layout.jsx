0ebb739e1831e6c1a6c9d4bbc610585f0b68677d
mohsensami <mohsensami18727@gmail.com>
2025-09-25 17:08:47 +0330

    property details component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import "@/assets/styles/globals.css";
+import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,3 +1,4 @@
+import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -27,6 +28,20 @@ const PropertyPage = async ({ params }) => {
           >
             <FaArrowLeft className="mr-2" /> Back to Properties
           </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={propertyDoc} />
+
+            {/* <!-- Sidebar --> */}
+            <aside className="space-y-4">
+              {/* <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} /> */}
+            </aside>
+          </div>
         </div>
       </section>
     </>
