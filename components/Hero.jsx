ffb877139ee4738877e91ad441d7e7af9193de8d
mohsensami <mohsensami18727@gmail.com>
2025-10-04 19:49:16 +0330

    property search component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,4 @@
+import PropertySearchForm from "./PropertySearchForm";
 
 const Hero = () => {
   return (
@@ -11,6 +12,7 @@ const Hero = () => {
             Discover the perfect property that suits your needs.
           </p>
         </div>
+        <PropertySearchForm />
       </div>
     </section>
   );
