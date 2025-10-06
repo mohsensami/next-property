db81cd2aecfb9dfc0dfd52cac0b238b5bf9347ed
mohsensami <mohsensami18727@gmail.com>
2025-10-06 11:19:48 +0330

    delete messages

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import markMessageAsRead from "@/app/actions/markMessageAsRead";
-// import deleteMessage from "@/app/actions/deleteMessage";
+import deleteMessage from "@/app/actions/deleteMessage";
 // import { useGlobalContext } from "@/context/GlobalContext";
 
 const MessageCard = ({ message }) => {
