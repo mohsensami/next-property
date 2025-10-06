ac419318eb77bcf6a4e6cdb5609daf648557746e
mohsensami <mohsensami18727@gmail.com>
2025-10-06 11:16:57 +0330

    mark as read

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { toast } from "react-toastify";
-// import markMessageAsRead from "@/app/actions/markMessageAsRead";
+import markMessageAsRead from "@/app/actions/markMessageAsRead";
 // import deleteMessage from "@/app/actions/deleteMessage";
 // import { useGlobalContext } from "@/context/GlobalContext";
 
